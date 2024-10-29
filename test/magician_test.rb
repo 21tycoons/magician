957c266b1ac1a9b96d2f76bdbc5fa141ee0d2b15
Liroy Leshed <liroyleshed@protonmail.com>
2024-10-28 21:01:46 -0700

    Update and rename wicked_test.rb to magician_test.rb

---
+++ test/magician_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class MagicianTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, Wicked
+  end
+
+  test "protected steps" do
+    error = assert_raise ProtectedStepError do
+      class ProtectedBadStepsController < ApplicationController
+        include Wicked::Wizard
+
+        steps :whatever, Magician::FINISH_STEP
+      end
+    end
+    assert_equal "Protected step detected: '#{Magician::FINISH_STEP}' is used internally by Wicked please rename your step", error.message
+  end
+end
